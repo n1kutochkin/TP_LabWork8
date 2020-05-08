0e567e5f4fce6537faf679bc58d0a4d8d4716870
n1kutochkin <nikutochkin28@gmail.com>
2020-05-09 01:45:17 +0300

    added TODO checklists

--- src/ru/mai/Main.java
+++ src/ru/mai/Main.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 //TODO Проставить везде модификаторы доступа для всех полей всех классов
+//TODO Прописать документацию по JavaDoc во всем проекте
 public class Main {
 
     public static void main(String[] args) {
